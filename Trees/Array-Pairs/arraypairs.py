bc527000d70eaef00691c7826a769cf815807110
Nikhil Kashyap <nikhilkashyap@Nikhils-MacBook-Air.local>
2020-05-06 22:52:13 +0530

    ArrayPairs

---
+++ Trees/Array-Pairs/arraypairs.py
@@ -0,0 +1,12 @@
+def solve(arr):
+    a = []
+    for i in range(0,len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i]* arr[j] <= max(arr[i:j+1]):
+                a.append(set([i+1,j+1]))
+    return len(a)
+
+if __name__ == '__main__':
+    arr = [1, 1, 2, 4, 2]
+    a = solve(arr)
+    print(a)
